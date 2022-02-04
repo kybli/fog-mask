b9d0d6feccd7b15d7eb1e1f858b7171588d342f6
Kyle Li <likyle001@gmail.com>
2022-02-04 02:18:51 -0600

    added transmission map refinement

--- mask.py
+++ mask.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-import skimage, skimage.io as io, skimage.morphology
+import skimage, skimage.io as io, skimage.morphology as morph, skimage.color as color, skimage.filters.rank as rank
 import cv2
 
 # value of 0 = masked
@@ -9,7 +9,7 @@ import cv2
 def dark_channel(img, size):
     dark_channel_img = np.amin(img, axis=2)
     footprint = np.ones((size, size))
-    dark_channel_img = skimage.morphology.erosion(dark_channel_img, footprint)
+    dark_channel_img = morph.erosion(dark_channel_img, footprint)
     return dark_channel_img
 
 def atmospheric_light(img, dark_channel_img):
@@ -34,6 +34,58 @@ def transmission_estimate(img, atm_light, size):
     w = 0.95
     return (255 * (1 - (w * dark_channel((img / atm_light), size)))).astype(np.uint8)
 
+def refine_transmission(img, transmission_estimate_img):
+    r = 30      # radius of kernel
+    e = 0.0001  # regularization
+
+    gray_scale_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY);
+    gray_scale_img = np.float64(gray_scale_img)/255;
+
+    transmission_estimate_img = transmission_estimate_img / 255
+
+    mean_I = cv2.boxFilter(gray_scale_img, cv2.CV_64F, (r,r))
+    mean_p = cv2.boxFilter(transmission_estimate_img, cv2.CV_64F, (r,r))
+    corr_I = cv2.boxFilter(gray_scale_img * gray_scale_img,cv2.CV_64F, (r,r))
+    corr_Ip = cv2.boxFilter(gray_scale_img * transmission_estimate_img, cv2.CV_64F, (r,r))
+
+    var_I   = corr_I - (mean_I * mean_I)
+    cov_Ip = corr_Ip - mean_I * mean_p
+
+    a = cov_Ip / (var_I + e)
+    b = mean_p - (a * mean_I)
+
+    mean_a = cv2.boxFilter(a, cv2.CV_64F, (r,r))
+    mean_b = cv2.boxFilter(b, cv2.CV_64F, (r,r))
+
+    q = (mean_a * gray_scale_img) + mean_b
+    return (q * 255).astype(np.uint8)
+
+
+    gray_scale_img = (255 * color.rgb2gray(img)).astype(np.uint8)
+    temp_gsi = color.rgb2gray(img)
+    footprint = morph.disk(r)
+
+    mean_I = rank.mean(gray_scale_img, footprint=footprint) / 255
+    mean_p = rank.mean(gray_scale_img, footprint=footprint) / 255
+    corr_I = rank.mean((gray_scale_img * (gray_scale_img / 255)).astype(np.uint8), footprint=footprint) / 255
+    corr_Ip = rank.mean((gray_scale_img * (transmission_estimate_img / 255)).astype(np.uint8), footprint=footprint) / 255
+
+    var_I = corr_I - (mean_I * mean_I)
+    cov_Ip = corr_Ip - (mean_I * mean_p)
+
+    a = (255 * (cov_Ip / (var_I + e))).astype(np.uint8)
+    a_temp = cov_Ip / (var_I + e)
+    b = (255 * ((mean_p - (a_temp * mean_I)))).astype(np.uint8)
+
+    mean_a = rank.mean(a, footprint=footprint) / 255
+    mean_b = rank.mean(b, footprint=footprint) / 255
+
+    # q = (mean_a * gray_scale_img / 1000000) + mean_b / 1000
+    q = (mean_a * temp_gsi) + mean_b
+
+    return (q * 255).astype(np.uint8)
+
+
 if __name__ == '__main__':
     file_path = "~/proj-x/fog-mask/img/input/foggy_cityscape.png"
 
@@ -43,6 +95,9 @@ if __name__ == '__main__':
     atm_light = atmospheric_light(img, dark_channel_img)
     transmission_estimate_img = transmission_estimate(img, atm_light, 15)
 
+    refined_transmission_img = refine_transmission(img, transmission_estimate_img)
+
     io.imsave('img/dark-channel/dark_channel.png', dark_channel_img)
     io.imsave('img/dark-channel/transmission_estimate.png', transmission_estimate_img)
+    io.imsave('img/dark-channel/refined_transmission.png', refined_transmission_img)
     
